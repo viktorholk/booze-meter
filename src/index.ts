aaa64bede5d710e4f2bf88a2cabcbcab23ddd5db
viktorholk <holkinator@gmail.com>
2023-12-14 13:00:47 +0100

    moved chart to express server and cleaned up project

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express, { Express, Request, Response } from "express";
-
-import DatabaseAdapter from "./utils/database-adapter";
-
-DatabaseAdapter.setupSchema();
-
+import path from 'path';
 const app: Express = express();
 const port = 3000;
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.set('views', path.join(__dirname, '/views'));
+app.set("view engine", "ejs");
+
+// index page
+app.get("/", function (req, res) {
+  res.render("pages/index");
+});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
